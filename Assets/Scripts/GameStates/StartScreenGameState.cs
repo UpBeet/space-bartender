7ad3f694ef75db6a76f3ea9ea669e9e55de196da
Weslo <wesley.rockholz@gmail.com>
2017-01-11 20:45:49 -0500

    Initial commit for placeholder start screen

---
+++ Assets/Scripts/GameStates/StartScreenGameState.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine.EventSystems;
+using YesAndEngine.GameStateManagement;
+
+namespace SpaceBartender.GameStates {
+
+	// Controller for the start screen game state.
+	public class StartScreenGameState : IGameState {
+
+		// Called when the player starts the game.
+		public void OnClickStartGame(PointerEventData e) {
+			Manager.PushStateAsync ("Gameplay");
+		}
+	}
+}
