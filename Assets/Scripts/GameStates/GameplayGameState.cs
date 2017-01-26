ef9833d6b688ddcaa1468459b7aad8112ba0d7e3
Weslo <wesley.rockholz@gmail.com>
2017-01-25 19:45:22 -0500

    Wipe this shit clean

--- Assets/Scripts/GameStates/GameplayGameState.cs
+++
@@ -1,8 +0,0 @@
-﻿using YesAndEngine.GameStateManagement;
-
-namespace SpaceBartender.GameStates {
-
-	// Controller for the gameplay game state.
-	public class GameplayGameState : IGameState {
-	}
-}
