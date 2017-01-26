43a902c74642e1a500aa62100ba0c37ecf0f64e1
Weslo <wesley.rockholz@gmail.com>
2017-01-25 20:41:31 -0500

    Move all code to PlayerController.cs and set up bar scene

--- Assets/Scripts/FirstPersonCamera.cs
+++ Assets/Scripts/FirstPersonCamera.cs
@@ -8,63 +8,12 @@ namespace SpaceBartender {
 	[RequireComponent(typeof(Camera))]
 	public class FirstPersonCamera : MonoBehaviour {
 
-		// Treshold for making sure the mouse doesn't snap around.
-		private const float SNAP_THRESHOLD = 0.5f;
-
-		// If set to true, inverts Y camera rotation.
-		[SerializeField]
-		private bool invertY = false;
-
-		// Horizontal rotation sensitivity.
-		[SerializeField]
-		private float horizontalSensitivity = 15f;
-
-		// Vertical rotation sensitivity.
-		[SerializeField]
-		private float verticalSensitivity = 15f;
-
-		// Minimum Y rotation
-		[SerializeField]
-		private float minRotationY = -60f;
-
-		// Maximum Y rotation.
-		[SerializeField]
-		private float maxRotationY = 60f;
-
-		// Cached Y rotation.
-		float rotY = 0;
-
 		// Initialize this component.
 		void Start () {
-			Cursor.lockState = CursorLockMode.Locked;
 		}
 
 		// Update this component between frames.
 		void Update () {
-
-			// Update rotation via mouse motion.
-			if(Cursor.lockState == CursorLockMode.Locked) {
-
-				// Get mouse axes.
-				float mouseX = Mathf.Clamp(Input.GetAxis ("Mouse X"), -SNAP_THRESHOLD, SNAP_THRESHOLD);
-				float mouseY = Mathf.Clamp(Input.GetAxis ("Mouse Y"), -SNAP_THRESHOLD, SNAP_THRESHOLD);
-
-				// Calculate rotation.
-				float rotX = transform.localEulerAngles.y + mouseX * horizontalSensitivity;
-				rotY = Mathf.Clamp (rotY + mouseY * verticalSensitivity, minRotationY, maxRotationY);
-
-				// Apply rotation.
-				transform.localEulerAngles = new Vector3 (rotY * (invertY ? 1 : -1), rotX, 0);
-			}
-
-			#if UNITY_EDITOR || DEBUG
-
-			// In editor/debug builds, toggle mouse lock with tilde key.
-			if(Input.GetKeyUp(KeyCode.BackQuote)) {
-				Cursor.lockState = Cursor.lockState == CursorLockMode.None ? CursorLockMode.Locked : CursorLockMode.None;
-			}
-
-			#endif
 		}
 	}
 }
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SpaceBartender {
+
+	// Player controller script.
+	public class PlayerController : MonoBehaviour {
+
+		// Treshold for making sure the mouse doesn't snap around.
+		private const float LOOK_SNAP_THRESHOLD = 0.5f;
+
+		// Movement speed.
+		[SerializeField]
+		private float movementSpeed = 3;
+
+		// If set to true, inverts Y camera rotation.
+		[SerializeField]
+		private bool invertLookY = false;
+
+		// Horizontal rotation look sensitivity.
+		[SerializeField]
+		private float horizontalLookSensitivity = 15f;
+
+		// Vertical rotation look sensitivity.
+		[SerializeField]
+		private float verticalLookSensitivity = 15f;
+
+		// Minimum Y look rotation
+		[SerializeField]
+		private float minLookRotationY = -60f;
+
+		// Maximum Y look rotation.
+		[SerializeField]
+		private float maxLookRotationY = 60f;
+
+		// Cached Y look rotation.
+		private float lookRotationY = 0;
+
+		// Reference to attached Camera component.
+		private Camera cam;
+
+		// Initialize this component.
+		void Start () {
+			Cursor.lockState = CursorLockMode.Locked;
+			cam = GetComponentInChildren<Camera> ();
+		}
+
+		// Update this component between frames.
+		void Update () {
+
+			float h = Input.GetAxis ("Horizontal");
+			float v = Input.GetAxis ("Vertical");
+
+			GetComponent<Rigidbody> ().velocity = transform.TransformDirection (h, 0, v) * movementSpeed;
+
+			// Update rotation via mouse motion.
+			if (Cursor.lockState == CursorLockMode.Locked) {
+
+				// Get mouse axes.
+				float mouseX = Mathf.Clamp (Input.GetAxis ("Mouse X"), -LOOK_SNAP_THRESHOLD, LOOK_SNAP_THRESHOLD);
+				float mouseY = Mathf.Clamp (Input.GetAxis ("Mouse Y"), -LOOK_SNAP_THRESHOLD, LOOK_SNAP_THRESHOLD);
+
+				// Calculate rotation.
+				float rotX = transform.localEulerAngles.y + mouseX * horizontalLookSensitivity;
+				lookRotationY = Mathf.Clamp (lookRotationY + mouseY * verticalLookSensitivity, minLookRotationY, maxLookRotationY);
+
+				// Apply rotation.
+				transform.localEulerAngles = new Vector3 (0, rotX, 0);
+				cam.transform.localEulerAngles = new Vector3 (lookRotationY * (invertLookY ? 1 : -1), 0, 0);
+			}
+
+			#if UNITY_EDITOR || DEBUG
+
+			// In editor/debug builds, toggle mouse lock with tilde key.
+			if (Input.GetKeyUp (KeyCode.BackQuote)) {
+				Cursor.lockState = Cursor.lockState == CursorLockMode.None ? CursorLockMode.Locked : CursorLockMode.None;
+			}
+
+			#endif
+		}
+	}
+}
