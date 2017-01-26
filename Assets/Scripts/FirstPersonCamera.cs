cb6fdf65049c296344588d5a04f4dc3f4affb6ed
Weslo <wesley.rockholz@gmail.com>
2017-01-25 20:07:08 -0500

    Add FirstPersonCamera.cs script

---
+++ Assets/Scripts/FirstPersonCamera.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SpaceBartender {
+
+	// Controls the first person camera.
+	[RequireComponent(typeof(Camera))]
+	public class FirstPersonCamera : MonoBehaviour {
+
+		// Horizontal rotation sensitivity.
+		[SerializeField]
+		private float horizontalSensitivity = 15f;
+
+		// Vertical rotation sensitivity.
+		[SerializeField]
+		private float verticalSensitivity = 15f;
+
+		// Minimum Y rotation
+		[SerializeField]
+		private float minRotationY = -60f;
+
+		// Maximum Y rotation.
+		[SerializeField]
+		private float maxRotationY = 60f;
+
+		// Cached Y rotation.
+		float rotY = 0;
+
+		// Initialize this component.
+		void Start () {
+			Cursor.lockState = CursorLockMode.Locked;
+		}
+
+		// Update this component between frames.
+		void Update () {
+
+			// Update rotation via mouse motion.
+			if(Cursor.lockState == CursorLockMode.Locked) {
+				float rotX = transform.localEulerAngles.y + Input.GetAxis ("Mouse X") * horizontalSensitivity;
+				rotY = Mathf.Clamp (rotY + Input.GetAxis ("Mouse Y") * verticalSensitivity, minRotationY, maxRotationY);
+				transform.localEulerAngles = new Vector3 (rotY, rotX, 0);
+			}
+
+			#if UNITY_EDITOR || DEBUG
+
+			// In editor/debug builds, toggle mouse lock with tilde key.
+			if(Input.GetKeyUp(KeyCode.BackQuote)) {
+				Cursor.lockState = Cursor.lockState == CursorLockMode.None ? CursorLockMode.Locked : CursorLockMode.None;
+			}
+
+			#endif
+		}
+	}
+}
